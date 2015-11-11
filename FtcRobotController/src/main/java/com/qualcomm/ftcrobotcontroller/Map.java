c73d1771ff9bbda0e233b2ea64bb47492cb437a0
Travis Fletcher <mindscape11@gmail.com>
2015-11-11 09:27:13 -0500

    Made Map class

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/Map.java
@@ -0,0 +1,50 @@
+package com.qualcomm.ftcrobotcontroller;
+
+/**
+ * Controls all map logic for autonomous
+ */
+
+//Map visualization
+//      {0,0,0,0,0,0,0,0,3,3,3,3}
+//      {0,0,0,0,0,0,0,0,0,3,3,3}
+//      {0,0,0,0,0,0,0,0,0,0,3,3}
+//      {0,0,0,0,0,0,0,0,0,0,0,3}
+//      {0,0,0,0,0,0,0,0,0,0,0,0}
+//      {0,0,0,0,0,0,0,0,0,0,0,0}
+//      {0,0,0,0,0,0,0,0,0,0,0,0}
+//      {3,0,0,0,0,0,0,0,0,0,0,0}
+//      {3,3,0,0,0,0,0,0,0,0,0,0}
+//      {3,3,3,0,0,0,0,0,0,0,0,0}
+//      {3,3,3,3,0,0,0,0,0,0,0,0}
+
+public class Map {
+    double goalX, goalY;
+    double robotX, robotY;
+    String teamColor;
+
+    public Map(String teamColor){ //pass in Team color
+        this.teamColor = teamColor;
+        if(teamColor.equals("Red")){
+            robotX = 7;
+            robotY = 11;
+        }else{
+            robotX = 11;
+            robotY = 7;
+        }
+    }
+    public void setGoal(int x, int y){
+        goalX = x;
+        goalY = y;
+    }
+    public double getGoalX(){
+        return goalX;
+    }
+    public double getGoalY(){
+        return goalY;
+    }
+    public double angleToGoal(){
+        double dX = robotX-goalX;
+        double dY = robotY-goalY;
+        return (Math.tan(dY/dX)*180)/Math.PI; //return degrees because that's what the gyro uses.
+    }
+}
