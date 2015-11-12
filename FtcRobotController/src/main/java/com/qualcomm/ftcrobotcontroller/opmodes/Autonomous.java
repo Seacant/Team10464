b01443f1e8409b93798ecbae8819d803efb5e113
Travis Fletcher <mindscape11@gmail.com>
2015-11-12 06:45:24 -0500

    Added base move states, and inserted temporary game statements to test

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
@@ -18,12 +18,16 @@ public class Autonomous extends OpMode {
     DcMotor motorLB;
     DcMotor motorA;
     DcMotor motorS;
-    GyroSensor gyro;
+//    GyroSensor gyro;
 
     //We stateful now, boys.
     int sensorState;
     int gameState;
     int moveState;
+    double power;
+    double sTime;
+    double eTime;
+    double dTime;
 
 //Map visualization
 //      {0,0,0,0,0,0,0,0,3,3,3,3}
@@ -41,7 +45,7 @@ public class Autonomous extends OpMode {
     Map map = new Map("Red"); //this map object will allow for easy manipulations.
 
     //GYRO
-    int xVal,yVal,zVal,heading;
+    double xVal,yVal,zVal,heading;
 
     public Autonomous() {
         //not used in the history of ever.
@@ -55,19 +59,19 @@ public class Autonomous extends OpMode {
     @Override
     public void init() {
         motorRT = hardwareMap.dcMotor.get("motor_RT");
-        motorRB = hardwareMap.dcMotor.get("motor_RT");
+        motorRB = hardwareMap.dcMotor.get("motor_RB");
 
         motorLT = hardwareMap.dcMotor.get("motor_LT");
-        motorLB = hardwareMap.dcMotor.get("motor_LT");
+        motorLB = hardwareMap.dcMotor.get("motor_LB");
 
-        motorLT.setDirection(DcMotor.Direction.REVERSE);
+        motorRT.setDirection(DcMotor.Direction.REVERSE);
         motorRB.setDirection(DcMotor.Direction.REVERSE);
 
         motorA = hardwareMap.dcMotor.get("motor_A");
         motorS = hardwareMap.dcMotor.get("motor_S");
 
-        gyro = hardwareMap.gyroSensor.get("gyro");
-        gyro.calibrate();
+//        gyro = hardwareMap.gyroSensor.get("gyro");
+//        gyro.calibrate();
     }
 
     /*
@@ -77,6 +81,7 @@ public class Autonomous extends OpMode {
      */
     @Override
     public void loop() {
+        sTime = getRuntime();
         //Information gathering phase
         switch(sensorState){
             case 0: //Start:
@@ -86,7 +91,8 @@ public class Autonomous extends OpMode {
                 //need, we can set sensorState to 0 and hope for the best.
                 break;
             case 1: //Color sensor logic
-                heading = gyro.getHeading();
+                if(motorLB.getPower()-motorRB.getPower()<=0){heading += 360*(36*dTime);}//gyro.getHeading();
+                if(motorLB.getPower()-motorRB.getPower()>=0){heading -= 360*(36*dTime);}//gyro.getHeading();
                 break;
             case 2:
                 //I'm now having the internal debate as to the importance of sensor states... It's
@@ -105,35 +111,65 @@ public class Autonomous extends OpMode {
                 //It was recommended to us that we should wait at the gate for a few second to allow
                 //our teammate to GTFO, avoiding unnecessary beginning-game collisions. It will also
                 //give our gyro a second to calibrate.
-                if(getRuntime() > 5 && !gyro.isCalibrating()) {
+//                if(getRuntime() > 5 && !gyro.isCalibrating()) {
                     gameState = 1;
-                }
+//                }
                 break;
-            case 1: //Colour sensor
+            case 1: //Move to beacon
                 //// TODO: 10/27/2015 Expand on gameState 1 uses
                 map.setGoal(0,7);
                 sensorState = 1;
+                //Checks our heading.
+                if(Math.abs(heading-map.angleToGoal()) < TOL){
+                    moveState = 1;
+                }else{
+                    moveState = 2;
+                }
+                break;
         }
         switch(moveState){
+            case 0:
+                //Case zero is 'stop'
+                motorRT.setPower(0);
+                motorRB.setPower(0);
+                motorLT.setPower(0);
+                motorLB.setPower(0);
+                break;
             //Never should we be just 'moving', always move TOWARDS something.
             case 1:
                 //Case one is 'move towards' in the most literal sense. It assumes the path is
                 //clear, and that there is a goal(9), and us(1) on the map somewhere.
-
-                //Checks our heading.
-                if(Math.abs(heading-map.angleToGoal()) < TOL){
-
-                }else{
-                    moveState = 2;
-                }
+                power = 0.5; //power coefficient
+                motorRT.setPower(power);
+                motorRB.setPower(power);
+                motorLT.setPower(power);
+                motorLB.setPower(power);
+                break;
             case 2:
-                //Case Two is 'turn towards'
+                //Case Two is 'turn towards'.
+                power = 0.5;
+                if(heading-map.angleToGoal()<0) {
+                    motorRT.setPower(power);
+                    motorRB.setPower(power);
+                    motorLT.setPower(-power);
+                    motorLB.setPower(-power);
+                }else{
+                    motorRT.setPower(-power);
+                    motorRB.setPower(-power);
+                    motorLT.setPower(power);
+                    motorLB.setPower(power);
+                }
+                break;
             case 3:
                 //Case Three is 'move around'. implies there is something in front of us that we'd
                 //like to not hit.
+                break;
         }
-
-
+        eTime = getRuntime();
+        dTime = eTime - sTime;
+        telemetry.addData("dTime ",dTime);
+        telemetry.addData("Runtime ",getRuntime());
+        telemetry.addData("heading ",heading);
     }
 
     /*
