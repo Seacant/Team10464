8b238e088ec7b478b01bbbab05d9ac0abde08346
Travis Fletcher <mindscape11@gmail.com>
2015-11-23 23:50:23 -0500

    Fixed moveRobot calculation, and added debug info(telemetry)

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
@@ -1,9 +1,10 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import android.util.Log;
+
 import com.qualcomm.ftcrobotcontroller.R;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.ftcrobotcontroller.Map;
 
@@ -14,10 +15,15 @@ import com.qualcomm.ftcrobotcontroller.Map;
  */
 
 public class Autonomous extends OpMode {
-    public final double TOL = .1; //tolerance for heading calculations
-    public final double DEGREES_TO_FEET = (20.45*.03281)/360; //Constant for converting encoder
-    //readings (degrees turned) into feet. **WARNING** Always calculate distance CHANGED, since
-    //encoders have no concept of direction, and we are moving across a 2D plane.
+    public final double TOL = 10; //tolerance for heading calculations
+    public final double DEGREES_TO_FEET =0.004659475694444444444444444444444444444444444444444;
+    // ((204.5*.03281)/360)*(360/1440) = Constant for converting encoder = ^^^^^^^^
+    //EXPLAINATION:
+    //204.5=65(diameter of sprocket in mm)*PI
+    //.03281 = millimeter to inch conversion
+    //360/1440 converts encoder reading, where one rotation is 1440, to degrees.
+    //**WARNING** Always calculate distance CHANGED, since encoders have no
+    // concept of direction, and we are moving across a 2D plane.
     DcMotor motorRT;
     DcMotor motorRB;
     DcMotor motorLT;
@@ -90,7 +96,9 @@ public class Autonomous extends OpMode {
     @Override
     public void loop() {
         //Information gathering phase
-        sTime = getRuntime();
+        sTime = eTime;
+        eTime = getRuntime();
+        dTime = eTime - sTime;
         lDist = cDist;
         cDist = (motorLB.getCurrentPosition()+motorRB.getCurrentPosition()+motorLT.getCurrentPosition()+motorLB.getCurrentPosition())/4; //average of motor positions
         dDist = cDist-lDist;
@@ -103,7 +111,7 @@ public class Autonomous extends OpMode {
                 //It was recommended to us that we should wait at the gate for a few second to allow
                 //our teammate to GTFO, avoiding unnecessary beginning-game collisions. It will also
                 //give our gyro a second to calibrate.
-                if(getRuntime() > 5 && !gyro.isCalibrating()) {
+                if(getRuntime() > 5 || !gyro.isCalibrating()) {
                     gameState = 1;
                 }
                 break;
@@ -111,11 +119,14 @@ public class Autonomous extends OpMode {
                 //// TODO: 10/27/2015 Expand on gameState 1 uses
                 map.setGoal(0, 7);
                 //Checks our heading.
-                if(Math.abs(heading-map.angleToGoal()) < TOL){
-                    moveState = 1;
-                }else{
-                    moveState = 2;
+                moveState = Math.abs(heading-map.angleToGoal()) < TOL ? 1 : 2;
+                if(map.distanceToGoal()<=.1) { //TODO: '|| colorsensor = white'
+                    moveState = 0;  // stop the robot
+                    gameState = 2;  // Move to the next stage.
                 }
+                break;
+            case 2:
+
                 break;
         }
         switch(moveState){
@@ -136,13 +147,13 @@ public class Autonomous extends OpMode {
                     motorRB.setPower(power);
                     motorLT.setPower(power);
                     motorLB.setPower(power);
-                    map.moveRobot(dDist*DEGREES_TO_FEET , heading);
+                    map.moveRobot(dDist * DEGREES_TO_FEET, heading);
                 }
                 break;
             case 2:
                 //Case Two is 'turn towards'.
                 power = 0.5;
-                if(heading-map.angleToGoal()<0) {
+                if(heading-map.angleToGoal()>0) {
                     motorRT.setPower(power);
                     motorRB.setPower(power);
                     motorLT.setPower(-power);
@@ -159,11 +170,16 @@ public class Autonomous extends OpMode {
                 //like to not hit.
                 break;
         }
-        eTime = getRuntime();
-        dTime = eTime - sTime;
+
         telemetry.addData("dTime ",dTime);
         telemetry.addData("Runtime ",getRuntime());
         telemetry.addData("heading ",heading);
+        telemetry.addData("cDist ",dDist);
+        telemetry.addData("goal x,y ",map.getGoalX()+","+map.getGoalY());
+        telemetry.addData("robot x,y ",map.getRobotX()+","+map.getRobotY());
+        telemetry.addData("angle to goal ",map.angleToGoal());
+        telemetry.addData("dist from goal ",map.distanceToGoal());
+        Log.i("Sand", "Distance traveled in " + dTime + ": " + (dDist * DEGREES_TO_FEET));
     }
 
     /*
